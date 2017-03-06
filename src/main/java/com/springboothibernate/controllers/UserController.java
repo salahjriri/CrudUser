d75a0799c2e01e9a5d6c3cda05fb390c1e4fa2f6
salahjriri <salah.jriri@gmail.com>
2017-03-06 11:15:40 +0100

    repo commit

--- src/main/java/com/springboothibernate/controllers/UserController.java
+++ src/main/java/com/springboothibernate/controllers/UserController.java
@@ -1,18 +1,20 @@
 package com.springboothibernate.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
 import com.springboothibernate.model.User;
 import com.springboothibernate.dao.UserService;
 
-@RestController
+@Controller
+@Path("/user")
 public class UserController {
 	@Autowired
 	User user;
@@ -20,41 +22,49 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@RequestMapping(value = "/getUsers", method = RequestMethod.GET)
+	@GET
+	@Path("/list")
+	@Produces("application/json")  
 	public List<User> getUsers(){
 		return userService.findAll();
 	}
-	@RequestMapping(value = "/addUser", method = RequestMethod.GET)
-	public User addUser(@RequestParam String users ){
-				String []userArry = users.split(",");
-				user.setUserName(userArry[0]);
-				user.setAge(Integer.valueOf(userArry[1]));
-				return userService.save(user);
+	
+	
+	@GET
+	@Path("/add/{userName}/{age}")
+	@Produces("application/json") 
+	public User addUser(@PathParam("userName") String userName,@PathParam("age") int age)
+	{
+		user.setAge(age);
+		user.setUserName(userName);
+		return userService.save(user);
 	}
-	@RequestMapping(value = "/updateUser", method = RequestMethod.GET)
-	public User updateUser(@RequestParam String users ){
-				String []userArry = users.split(",");
-				
-				if(userService.exists(Integer.valueOf(userArry[0]))){
-				user = userService.findOne(Integer.valueOf(userArry[0]));
-				user.setUserName(userArry[1]);
-				user.setAge(Integer.valueOf(userArry[2]));
-				return userService.save(user);
-				}
-				else {
-					user.setUserName(userArry[0]);
-					user.setAge(Integer.valueOf(userArry[1]));
-					return userService.save(user);
-				}
+	@GET
+	@Path("/update/{userId}/{userName}/{age}")
+	@Produces("application/json") 
+	public User UpdateUser(@PathParam("userId") int userId ,@PathParam("userName") String userName,@PathParam("age") int age)
+	{
+		
+		
+		if(userService.exists(userId)){
+			user = userService.findOne(userId);
+			user.setUserName(userName);
+			user.setAge(age);
+		userService.save(user);}
+		return user;
+	}
+	@GET
+	@Path("/deleteUser/{userId}")
+	public void DeleteUser(@PathParam("userId") int userId )
+	{
+		if(userService.exists(userId)){
+			userService.delete(userId);
 		}
-			
-	@RequestMapping(value = "/deleteUser", method = RequestMethod.GET)
-	public void deleteUser(@RequestParam String userId){
-		if(userId.equals(null)){ return ;}
-		else {
-		String[] b = userId.split(",");
-		if(userService.exists(Integer.valueOf(b[0])))
-		userService.delete(Integer.valueOf(b[0]));
+	
 	}
 	
-}}
+	
+	
+
+	
+}
